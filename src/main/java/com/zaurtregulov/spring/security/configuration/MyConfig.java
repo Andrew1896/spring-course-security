e067828ba79c875aa6e6c5b43730cbad53760066
Andrei Cataraga <andreicataraga18@gmail.com>
2024-03-07 23:37:15 +0100

    Saving the password in the Database

--- src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MyConfig.java
@@ -1,5 +1,6 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
-import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.zaurtregulov.spring.security")
@@ -24,39 +24,17 @@ public class MyConfig {
         return internalResourceViewResolver;
     }
 
-//    @Bean
-//    public DataSource dataSource() {
-//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-//        try {
-//            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-//            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
-//            dataSource.setUser("root");
-//            dataSource.setPassword("bestuser");
-//        } catch (PropertyVetoException e) {
-//            e.printStackTrace();
-//        }
-//        return dataSource;
-//    }
-//
-//    @Bean
-//    public LocalSessionFactoryBean sessionFactory() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//        sessionFactory.setDataSource(dataSource());
-//        sessionFactory.setPackagesToScan("com.zaurtregulov.spring.rest.entity");
-//
-//        Properties hibernateProperties = new Properties();
-//        hibernateProperties.setProperty("hibernate.dialect",
-//                "org.hibernate.dialect.MySQLDialect");
-//        hibernateProperties.setProperty("hibernate.show_sql", "true");
-//        sessionFactory.setHibernateProperties(hibernateProperties);
-//        return sessionFactory;
-//    }
-//
-//    @Bean
-//    public HibernateTransactionManager transactionManager() {
-//        HibernateTransactionManager transactionManager =
-//                new HibernateTransactionManager();
-//        transactionManager.setSessionFactory(sessionFactory().getObject());
-//        return transactionManager;
-//    }
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("root");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
 }
--- src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,21 +1,30 @@
 package com.zaurtregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("Andrei").password("Andrei18").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("Tudor").password("Tudor09").roles("HR"))
-                .withUser(userBuilder.username("Maria").password("Maria24").roles("MANAGER"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+//        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("Andrei").password("Andrei18").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("Tudor").password("Tudor09").roles("HR"))
+//                .withUser(userBuilder.username("Maria").password("Maria24").roles("MANAGER"));
     }
 
     @Override
